15c1e57f7321cbfea593a4339135ef48af78edd8
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-01-28 16:03:44 +0300

    Initial Commit

---
+++ LottieMagic-İOS/LottieMagic_I_OSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieMagic_I_OSApp.swift
+//  LottieMagic-İOS
+//
+//  Created by Bartuğ Kaan Çelebi on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct LottieMagic_I_OSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
